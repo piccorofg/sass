2d8554362a54294f06edc1ac29bcc8b8b15d079e
piccorofg <casof_1@hotmail.com>
2022-05-12 19:26:46 -0500

    changes to master

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,43 @@
-var gulp = require ('gulp');                                   // llamar a gulp
-var sass = requirte ('gulp-sass');                             // llamar desde gulp a sass 
-var sourcemaps = require ('gulp-sourcemaps');                  // .map para relacionar sass con css
-var browserSync = require ('browser-sync').create();           // el navegador se actualice con los últimos cambios que se van generando
+var gulp = require('gulp'); // llamar a gulp
+var sass = require('gulp-sass'); // llamar desde gulp a sass 
+var sourcemaps = require('gulp-sourcemaps'); // .map para relacionar sass con css
+var browserSync = require('browser-sync').create(); // el navegador se actualice con los últimos cambios que se van generando
 
-gulp.task('default', gulp.series(), function () {});
+/*
+** 
+DEFINIR DONDE ESTÁN LOS ARCHIVOS SASS Y TRABAJAR CON LOS SOURCEMAP
+DEFINIR LOS PUNTOS DE DESTINO EN LA CARPETA CSS 
+*/
+
+gulp.task('sass',  function(){
+    return gulp.src('./sass/**/*sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass( options, {}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+/*
+** 
+SINCRONIZAR CON UN BROWSER 
+CONTROL DE CAMBIOS CON GULP EN SASS
+*/
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('./sass/**/*.sass', gulp.series('sass'));
+});
+
+
+/*
+**  
+ Ejecutar gulp desde terminal, serie tareas seguidas
+*/
+gulp.task('default', gulp.series('sass', 'serve'), function () {});
+
+
